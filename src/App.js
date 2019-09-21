78289995c3978ac68810a9e12c262e470e8e8d38
Charles <charles.neblett@gmail.com>
2019-09-21 14:33:10 -0500

    styling a logo

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import logo from './logo.png'
 import tournaments from './data/tournaments.js'
 import { BrowserRouter, Route } from 'react-router-dom';
 import UserLogin from './components/UserLogin';
@@ -7,6 +8,8 @@ import Lobby from './components/Lobby';
 import Dashboard from './components/Dashboard';
 // import SubmissionPortal from './components/SubmissionPortal';
 // import PrizePayout from './components/PrizePayout';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 class App extends React.Component {
   constructor(props) {
@@ -30,9 +33,14 @@ class App extends React.Component {
 
     return (
       <div className="App">
-        <br></br>
-        <h1 style={{textAlign: "center"}} >MiniHacks</h1>
-        <br></br>
+        <Row>
+          <Col></Col>
+          <Col md="auto">
+            <img style={{textAlign: "center"}} src={logo} alt="logo" />
+            <br></br>
+          </Col>
+          <Col></Col>
+        </Row>
         <BrowserRouter>
           <Route exact path="/" component={UserLogin}/>
           <Route path="/Lobby" render={() => <Lobby tournaments={tournaments} />}/>
