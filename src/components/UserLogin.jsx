81b104f3f686e7e819256e1e14a646b1d0811d0f
Geoff Langenderfer <geoff.langenderfer@gmail.com>
2019-09-21 14:49:23 -0500

    added user type to login screen

--- src/components/UserLogin.jsx
+++ src/components/UserLogin.jsx
@@ -1,34 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import { Link } from 'react-router-dom';
 
 
 const UserLogin = () => {
+  const [userType, setUserType] = useState('');
   return (
     <Container>
       <Form>
-      <Form.Group controlId="formBasicEmail">
-      <Form.Label>Email address</Form.Label>
-      <Form.Control type="email" placeholder="Enter email" />
-      <Form.Text className="text-muted">
-      We'll never share your email with anyone else.
-      </Form.Text>
-      </Form.Group>
-
-      <Form.Group controlId="formBasicPassword">
-      <Form.Label>Password</Form.Label>
-      <Form.Control type="password" placeholder="Password" />
-      </Form.Group>
-      <Form.Group controlId="formBasicCheckbox">
-      <Form.Check type="checkbox" label="Check me out" />
-      </Form.Group>
-      <Link to="/Lobby">
-        <Button variant="primary" type="submit">
-        Submit
-        </Button>
-      </Link>
+        <Form.Group as={Row} controlId="formHorizontalEmail">
+          <Form.Label column sm={2}>
+            Email
+          </Form.Label>
+          <Col sm={10}>
+            <Form.Control type="email" placeholder="Email" />
+          </Col>
+        </Form.Group>
+      
+        <Form.Group as={Row} controlId="formHorizontalPassword">
+          <Form.Label column sm={2}>
+            Password
+          </Form.Label>
+          <Col sm={10}>
+            <Form.Control type="password" placeholder="Password" />
+          </Col>
+        </Form.Group>
+        <fieldset>
+          <Form.Group as={Row}>
+            <Form.Label as="legend" column sm={2}>
+              I am a:
+            </Form.Label>
+            <Col sm={10}>
+              <Form.Check
+                type="radio"
+                label="Competitor"
+                onClick={() => setUserType("Competitor")}
+                name="Competitor"
+                id="Competitor"
+              />
+              <Form.Check
+                type="radio"
+                label="Judge"
+                onClick={() => setUserType("Judge")}
+                name="Judge"
+                id="Judge"
+              />
+            </Col>
+          </Form.Group>
+        </fieldset>
+        <Form.Group as={Row}>
+          <Col sm={{ span: 10, offset: 2 }}>
+            <Link to={userType + "Lobby"}>
+              <Button type="submit">Sign in</Button>
+            </Link>
+          </Col>
+        </Form.Group>
       </Form>
     </Container>
   );
