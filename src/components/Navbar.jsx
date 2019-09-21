54eeac269ebe4479ed9507c3f810f0fdcc84df00
Charles <charles.neblett@gmail.com>
2019-09-21 18:06:17 -0500

    test

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,12 +6,12 @@ import {Link} from 'react-router-dom';
 const Navibar = props => {
   return (
     <Navbar bg="primary" variant="dark">
-    <Nav className="mr-auto">
-      <Nav.Link><Link style={{color: "white"}} to="/Lobby">Lobby</Link></Nav.Link>
-      <Nav.Link><Link style={{color: "white"}} to="/MyMiniHacks">My MiniHacks</Link></Nav.Link>
-      <Nav.Link><Link style={{color: "white"}} to="/CreateMiniHack">Create a MiniHack</Link></Nav.Link>
-    </Nav>
-  </Navbar>
+      <Nav className="mr-auto">
+        <Nav.Link><Link style={{color: "white"}} to="/Lobby">Lobby</Link></Nav.Link>
+        <Nav.Link><Link style={{color: "white"}} to="/MyMiniHacks">My MiniHacks</Link></Nav.Link>
+        <Nav.Link><Link style={{color: "white"}} to="/CreateMiniHack">Create a MiniHack</Link></Nav.Link>
+      </Nav>
+    </Navbar>
   )
 }
 
