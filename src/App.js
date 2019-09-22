4238ebb1240eec9b81dddb7dc60a54abc661a4ff
Charles <charles.neblett@gmail.com>
2019-09-22 13:20:56 -0500

    final for hackathon

--- src/App.js
+++ src/App.js
@@ -138,32 +138,6 @@ class App extends React.Component {
     this.setState({tournaments : tournaments});
  }
 
- /*
-
-    name: 'WWC Diversity Hackathon 5',
-    prize: 1000,
-    start_time: 'Sun, Sep 22 @ 8:00AM',
-    max_competitors: 30,
-    competitors: [
-      'name1',
-      'name2',
-      'name3',
-      'name4'
-    ],
-    information: {
-      description: `This hackathon is a celebration of diversity. Anyone who supports diversity is welcome — no matter your level of technical knowledge. 
-      We are providing a space for people of all backgrounds who want to start building their tech portfolio or finish up a tech project while learning new skills. Previous experience in tech is **NOT** required. 
-      Plus, we have a parallel FREE workshop track for anyone who wants to come and just start learning, learn more, or meet people. 
-      Not sure what to work on? We encourage participants to look into open data or civic hacking projects, for more information check out Open Austin.`,
-      media: '',
-      requirements: [
-        'Support each other',
-        'Be creative',
-        'Have fun'
-      ]
-    }
- */
-
   render() {
 
     const {tournaments, clickedTournament, registeredTournaments, emailText, passwordText, user, type, requirementsCounter} = this.state
